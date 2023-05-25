e8ee3fccfc423befbc56f9c0488303235ec08bc8
silia6375 <ceid6375@upnet.gr>
2023-05-26 01:46:47 +0300

    new class event

---
+++ codev0.1/event.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Event {
+    private int eventID;
+    private int restaurantID;
+    private String name;
+    private String description;
+    private Date start_time;
+    private Date end_time;
+}
