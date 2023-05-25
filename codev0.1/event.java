856376f09f7cbb6e7950caa8be1029fe718542ee
silia6375 <ceid6375@upnet.gr>
2023-05-26 01:53:10 +0300

    constructor

--- codev0.1/event.java
+++ codev0.1/event.java
@@ -9,4 +9,17 @@ public class Event {
     private String description;
     private Date start_time;
     private Date end_time;
+    
+    private static Map<Integer, Event> eventMap = new HashMap<>(); // Map to store events by eventID
+    
+    // Constructor
+    public Event(int eventID, int restaurantID, String name, String description, Date start_time, Date end_time) {
+        this.eventID = eventID;
+        this.restaurantID = restaurantID;
+        this.name = name;
+        this.description = description;
+        this.start_time = start_time;
+        this.end_time = end_time;
+    }
+
 }
