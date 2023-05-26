ad2b7ac0e58c3e85bf697c443ac8e6e88649b0a9
silia6375 <ceid6375@upnet.gr>
2023-05-26 03:00:24 +0300

    public class and constructor

--- codev0.1/offer.java
+++ codev0.1/offer.java
@@ -1,3 +1,22 @@
-public class offer {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Offer {
+    private int id;
+    private int restaurantID;
+    private String name;
+    private String description;
+    private float discount;
+    private float minSpent;
     
-}
+    private static Map<Integer, Offer> offerMap = new HashMap<>(); // Map to store offers by id
+    
+    // Constructor
+    public Offer(int id, int restaurantID, String name, String description, float discount, float minSpent) {
+        this.id = id;
+        this.restaurantID = restaurantID;
+        this.name = name;
+        this.description = description;
+        this.discount = discount;
+        this.minSpent = minSpent;
+    }
