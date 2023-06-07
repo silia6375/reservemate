b6b2a1fbcf0ccdeeeed9608a889b3361a9bc764c
silia6375 <ceid6375@upnet.gr>
2023-06-07 03:49:22 +0300

    correcting errors

--- codev0.1/offer.java
+++ codev0.1/offer.java
@@ -12,11 +12,13 @@ public class offer {
     private float discount;
     private float minSpent;
     private String couponcode;
+    private String city;
+    private String restaurantName;
     
     private static Map<Integer, offer> offerMap = new HashMap<>(); // Map to store offers by id
     
     // Constructor
-    public Offer(int id, int restaurantID, String name, String description, float discount, float minSpent, String couponcode) {
+    public offer(int id, int restaurantID, String name, String description, float discount, float minSpent, String couponcode, String city, String restaurantName) {
         this.id = id;
         this.restaurantID = restaurantID;
         this.name = name;
@@ -24,18 +26,20 @@ public class offer {
         this.discount = discount;
         this.minSpent = minSpent;
         this.couponcode = couponcode;
+        this.city = city;
+        this.restaurantName = restaurantName;
 
     }
     // Method to add a new offer
-    public static void addNewOffer(int id, int restaurantID, String name, String description, float discount, float minSpent, String couponcode) {
-        Offer offer = new Offer(id, restaurantID, name, description, discount, minSpent, couponcode);
+    public static void addNewOffer(int id, int restaurantID, String name, String description, float discount, float minSpent, String couponcode, String city, String restaurantName) {
+        offer offer = new offer(id, restaurantID, name, description, discount, minSpent, couponcode, city, restaurantName);
         offerMap.put(id, offer);
         System.out.println("Offer added successfully.");
 
     }
 // Method to update the discount and/or minSpent of an offer using id
 public static void updateOfferInfo(int id, float newDiscount, float newMinSpent) {
-    Offer offer = offerMap.get(id);
+    offer offer = offerMap.get(id);
     if (offer != null) {
         if (newDiscount >= 0) {
             offer.setDiscount(newDiscount);
@@ -52,7 +56,7 @@ public static void updateOfferInfo(int id, float newDiscount, float newMinSpent)
 
  // Method to delete an offer using id
     public static void deleteOffer(int id) {
-        Offer offer = offerMap.remove(id);
+        offer offer = offerMap.remove(id);
         if (offer != null) {
             System.out.println("Offer deleted successfully.");
         } else {
@@ -64,7 +68,7 @@ public static void updateOfferInfo(int id, float newDiscount, float newMinSpent)
     // Method to get all cities with offers
     public static List<String> getCitiesWithOffers() {
         List<String> citiesWithOffers = new ArrayList<>();
-        for (Offer offer : offerMap.values()) {
+        for (offer offer : offerMap.values()) {
             if (!citiesWithOffers.contains(offer.getCity())) {
                 citiesWithOffers.add(offer.getCity());
             }
@@ -75,7 +79,7 @@ public static void updateOfferInfo(int id, float newDiscount, float newMinSpent)
     // Method to get all restaurants in a selected city with offers
 public static List<Integer> getRestaurantsInCityWithOffers(String city) {
     List<Integer> restaurantsInCityWithOffers = new ArrayList<>();
-    for (Offer offer : offerMap.values()) {
+    for (offer offer : offerMap.values()) {
         if (offer.getCity().equalsIgnoreCase(city)) {
             restaurantsInCityWithOffers.add(offer.getRestaurantID());
         }
@@ -83,17 +87,14 @@ public static List<Integer> getRestaurantsInCityWithOffers(String city) {
     return restaurantsInCityWithOffers;
 }
 // Method to get offers of a selected restaurant
-public static List<Offer> getOffersOfRestaurant(int restaurantID) {
-    List<Offer> offersOfRestaurant = new ArrayList<>();
-    for (Offer offer : offerMap.values()) {
+public static List<offer> getOffersOfRestaurant(int restaurantID) {
+    List<offer> offersOfRestaurant = new ArrayList<>();
+    for (offer offer : offerMap.values()) {
         if (offer.getRestaurantID() == restaurantID) {
             offersOfRestaurant.add(offer);
         }
     }
     return offersOfRestaurant;
-}
-
-
 }
 
 // Getter and Setter methods for the restaurantName variable
@@ -148,22 +149,22 @@ public String getCity() {
 
 public static void main(String[] args) {
     // Adding offers
-    Offer.addNewOffer(1, 1, "Discounted Pizza", "Get 20% off on all pizzas", "PIZZA20", 20.0f, 50.0f, "New York", "Pizza Palace");
-    Offer.addNewOffer(2, 2, "Lunch Special", "Special lunch combo for just $10", "LUNCH10", 0.0f, 10.0f, "Chicago", "Burger Bistro");
-    Offer.addNewOffer(3, 1, "Free Dessert", "Get a free dessert with any order", "DESSERTFREE", 0.0f, 0.0f, "New York", "Pizza Palace");
-    Offer.addNewOffer(4, 3, "Family Meal Deal", "Enjoy a family meal for only $30", "FAMILY30", 10.0f, 30.0f, "Los Angeles", "Mexican Grill");
-    Offer.addNewOffer(5, 4, "Happy Hour Drinks", "Half-price drinks from 5 PM to 7 PM", "HAPPYHOUR", 50.0f, 0.0f, "San Francisco", "Cocktail Lounge");
-    Offer.addNewOffer(6, 2, "Breakfast Buffet", "All-you-can-eat breakfast buffet for $15", "BREAKFAST15", 0.0f, 15.0f, "Chicago", "Burger Bistro");
-    Offer.addNewOffer(7, 5, "Weekend Brunch", "Brunch specials every Saturday and Sunday", "BRUNCH20", 20.0f, 0.0f, "Miami", "Cafe Delight");
-    Offer.addNewOffer(8, 6, "Combo Meal Deal", "Get a burger, fries, and a drink for $8", "COMBO8", 0.0f, 8.0f, "Seattle", "Fast Bites");
-    Offer.addNewOffer(9, 7, "Student Discount", "10% off for students with valid ID", "STUDENT10", 10.0f, 0.0f, "Boston", "Diner's Delight");
-    Offer.addNewOffer(10, 8, "Weekend Special", "Exclusive weekend offer for our loyal customers", "WEEKEND20", 20.0f, 0.0f, "New York", "The Steakhouse");
+    offer.addNewOffer(1, 1, "Discounted Pizza", "Get 20% off on all pizzas", "PIZZA20", 20.0f, 50.0f, "New York", "Pizza Palace");
+    offer.addNewOffer(2, 2, "Lunch Special", "Special lunch combo for just $10", "LUNCH10", 0.0f, 10.0f, "Chicago", "Burger Bistro");
+    offer.addNewOffer(3, 1, "Free Dessert", "Get a free dessert with any order", "DESSERTFREE", 0.0f, 0.0f, "New York", "Pizza Palace");
+    offer.addNewOffer(4, 3, "Family Meal Deal", "Enjoy a family meal for only $30", "FAMILY30", 10.0f, 30.0f, "Los Angeles", "Mexican Grill");
+    offer.addNewOffer(5, 4, "Happy Hour Drinks", "Half-price drinks from 5 PM to 7 PM", "HAPPYHOUR", 50.0f, 0.0f, "San Francisco", "Cocktail Lounge");
+    offer.addNewOffer(6, 2, "Breakfast Buffet", "All-you-can-eat breakfast buffet for $15", "BREAKFAST15", 0.0f, 15.0f, "Chicago", "Burger Bistro");
+    offer.addNewOffer(7, 5, "Weekend Brunch", "Brunch specials every Saturday and Sunday", "BRUNCH20", 20.0f, 0.0f, "Miami", "Cafe Delight");
+    offer.addNewOffer(8, 6, "Combo Meal Deal", "Get a burger, fries, and a drink for $8", "COMBO8", 0.0f, 8.0f, "Seattle", "Fast Bites");
+    offer.addNewOffer(9, 7, "Student Discount", "10% off for students with valid ID", "STUDENT10", 10.0f, 0.0f, "Boston", "Diner's Delight");
+    offer.addNewOffer(10, 8, "Weekend Special", "Exclusive weekend offer for our loyal customers", "WEEKEND20", 20.0f, 0.0f, "New York", "The Steakhouse");
 
 
     Scanner scanner = new Scanner(System.in);
 
     // Step 1: Display all cities with offers
-    List<String> citiesWithOffers = Offer.getCitiesWithOffers();
+    List<String> citiesWithOffers = offer.getCitiesWithOffers();
     System.out.println("Cities with offers: ");
     for (String city : citiesWithOffers) {
         System.out.println(city);
@@ -174,10 +175,10 @@ public static void main(String[] args) {
     String selectedCity = scanner.nextLine();
 
     // Step 3: Display all restaurants in the selected city with offers
-    List<Integer> restaurantsWithOffers = Offer.getRestaurantsInCityWithOffers(selectedCity);
+    List<Integer> restaurantsWithOffers = offer.getRestaurantsInCityWithOffers(selectedCity);
     System.out.println("Restaurants in " + selectedCity + " with offers: ");
     for (int restaurantID : restaurantsWithOffers) {
-        Offer offer = Offer.offerMap.get(restaurantID);
+        offer offer = offer.offerMap.get(restaurantID);
         System.out.println(offer.getRestaurantName());
     }
 
@@ -188,7 +189,7 @@ public static void main(String[] args) {
     int selectedRestaurantID = -1;
 
     // Find the restaurant ID based on the selected restaurant name
-    for (Offer offer : Offer.offerMap.values()) {
+    for (offer offer : offer.offerMap.values()) {
         if (offer.getRestaurantName().equalsIgnoreCase(selectedRestaurantName)) {
             selectedRestaurantID = offer.getRestaurantID();
             break;
@@ -196,12 +197,12 @@ public static void main(String[] args) {
     }
 
     // Step 5: Display list of offers of the selected restaurant
-    List<Offer> offersOfSelectedRestaurant = Offer.getOffersOfRestaurant(selectedRestaurantID);
+    List<offer> offersOfSelectedRestaurant = offer.getOffersOfRestaurant(selectedRestaurantID);
     if (offersOfSelectedRestaurant.isEmpty()) {
         System.out.println("Sorry, there are no offers for this restaurant.");
     } else {
         System.out.println("Offers of " + selectedRestaurantName + ": ");
-        for (Offer offer : offersOfSelectedRestaurant) {
+        for (offer offer : offersOfSelectedRestaurant) {
             System.out.println("- " + offer.getName());
         }
     }
@@ -211,8 +212,8 @@ public static void main(String[] args) {
     String selectedOfferName = scanner.nextLine();
 
     // Step 7: The system prints all the information of the selected offer
-    Offer selectedOffer = null;
-    for (Offer offer : offersOfSelectedRestaurant) {
+    offer selectedOffer = null;
+    for (offer offer : offersOfSelectedRestaurant) {
         if (offer.getName().equalsIgnoreCase(selectedOfferName)) {
             selectedOffer = offer;
             break;
